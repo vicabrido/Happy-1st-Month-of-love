ac17c8dee7ed094e4abacec058efe100e7ede411
vicabrido <vincentcabrido83@gmail.com>
2025-09-24 18:02:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 onload = () => {
     document.body.classList.remove("container");
-  };
+};
 
-  const wrapper = document.querySelector(".wrapper");
-  const openBtn = document.getElementById("openBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  
-  openBtn.addEventListener("click", () => {
-      wrapper.classList.add("open");
-      openBtn.style.display = "none";
-      closeBtn.style.display = "inline-block";
-  });
-  
-  closeBtn.addEventListener("click", () => {
-      wrapper.classList.remove("open");
-      closeBtn.style.display = "none";
-      openBtn.style.display = "inline-block";
-  });
-  
+const wrapper = document.querySelector(".wrapper");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const bgMusic = document.getElementById("bgMusic");
+
+openBtn.addEventListener("click", () => {
+    wrapper.classList.add("open");
+    openBtn.style.display = "none";
+    closeBtn.style.display = "inline-block";
+
+    // Play music
+    bgMusic.currentTime = 0;
+    bgMusic.play();
+});
+
+closeBtn.addEventListener("click", () => {
+    wrapper.classList.remove("open");
+    closeBtn.style.display = "none";
+    openBtn.style.display = "inline-block";
+
+    // Stop music
+    bgMusic.pause();
+    bgMusic.currentTime = 0;
+});
